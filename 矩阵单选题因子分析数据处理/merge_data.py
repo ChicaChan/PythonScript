f62b2225bd9fce6ea474ef2cca9f79f96f568703
ChicaChan <2789005462@qq.com>
2024-11-28 22:20:05 +0800

    因子处理增加多题同时处理

--- 矩阵单选题因子分析数据处理/merge_data.py
+++ 矩阵单选题因子分析数据处理/merge_data.py
@@ -4,23 +4,39 @@ df = pd.read_excel('data.xlsx')
 
 
 def process_column_name(col_name):
-    # 拆分列名，获取题号.品牌序号_描述
-    question_brand, description = col_name.split('_')
-    question = question_brand.split('.')[0]
-    return f"{question}_{description}"
+    # 检查列名是否包含 '_'
+    if '_' in col_name:
+        # 拆分列名，获取题号.品牌序号_描述
+        question_brand, description = col_name.split('_')
+        question = question_brand.split('.')[0]
+        return f"{question}_{description}"
+    else:
+        return col_name
 
 
 merged_columns = {}
+no_underscore_columns = []
+
 
 for col in df.columns:
     new_col = process_column_name(col)
 
-    if new_col not in merged_columns:
-        merged_columns[new_col] = df[col]
+    if new_col != col:
+        if new_col not in merged_columns:
+            merged_columns[new_col] = df[col]
+        else:
+            merged_columns[new_col] += df[col]
     else:
-        merged_columns[new_col] += df[col]
+        no_underscore_columns.append(col)
 
 merged_df = pd.DataFrame(merged_columns)
+
+for col in no_underscore_columns:
+    merged_df[col] = df[col]
+
+if no_underscore_columns:
+    print("没有'_'的列有：")
+    print(no_underscore_columns)
 
 print("合并后的数据：")
 print(merged_df.head())
@@ -28,6 +44,7 @@ print(merged_df.head())
 # 检查哪些列的总和为0
 zero_sum_columns = merged_df.columns[merged_df.sum() == 0].tolist()
 
+# 输出总和为0的列
 if zero_sum_columns:
     print("总和为0的列有：")
     print(zero_sum_columns)
