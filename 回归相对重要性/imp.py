9dc35d934ff234a8fe3b59b59086c5b561e875eb
ChicaChan <2789005462@qq.com>
2025-03-17 18:19:40 +0800

    0317更新

--- 回归相对重要性/imp.py
+++ 回归相对重要性/imp.py
@@ -2,7 +2,7 @@
 import pandas as pd
 from relativeImp import relativeImp
 
-df = pd.read_excel("C:\\Users\\Qdtech\\Downloads\\table-v2.xlsx")
+df = pd.read_csv("C:\\Users\\Qdtech\\Downloads\\字节支付NPS-25Q1-驱动数据-B12-C组.csv", encoding='utf-8')
 yName = 'C001'
 xNames = ['C1R1',
           'C2R1',
@@ -14,5 +14,5 @@ xNames = ['C1R1',
           'C16R1',
           'C18R1']
 
-df_results = relativeImp(df, outcomeName=yName, driverNames=xNames)
+df_results = relativeImp(df, outcomeName=yName, driverNames=xNames).sort_values(by='rawRelaImpt', ascending=False)
 print(df_results)
